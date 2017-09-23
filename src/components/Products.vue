a9df3aebd1fe8d40c8f502a3c1ce4dbb7c921d18
Diogo Cezar <diogo@diogocezar.com>
2017-09-23 18:33:49 -0300

    Ajustes

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,31 +1,30 @@
 <template>
-  <div class="products">
-    <h1>Produto {{ id }}</h1>
-    <h2>{{ subtitle }}</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et possimus facilis, voluptatem aliquam amet provident similique doloremque qui nobis rem, asperiores quis ab reiciendis ea dolore, praesentium cumque eveniet nemo!</p>
-  </div>
+	<div class="products">
+		<h1>Produto {{ id }}</h1>
+		<h2>{{ subtitle }}</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et possimus facilis, voluptatem aliquam amet provident similique doloremque qui nobis rem, asperiores quis ab reiciendis ea dolore, praesentium cumque eveniet nemo!</p>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'products',
-  data () {
-    return {
-      subtitle: 'Página de Produtos',
-      id : this.$route.params.id
-    }
-  },
-  watch: {
-    '$route': 'changeId'
-  },
-  methods: {
-    changeId(){
-      this.$data.id = this.$route.params.id;
-    }
-  }
+	name: 'products',
+	data () {
+		return {
+			subtitle: 'Página de Produtos',
+			id : this.$route.params.id
+		}
+	},
+	watch: {
+		'$route': 'changeId'
+	},
+	methods: {
+		changeId(){
+			this.$data.id = this.$route.params.id;
+		}
+	}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
